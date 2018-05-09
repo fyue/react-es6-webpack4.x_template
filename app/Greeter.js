ac691f66b097c28ac6c429354907266dc5da26b5
fyue(GongJinJun) <544648788@qq.com>
2018-05-09 14:20:17 +0800

    '基本的loader使用可以了哦'

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,5 +1,16 @@
-module.exports = function() {
-    var greet = document.createElement('div');
-    greet.textContent = "Hi there and greetings!";
-    return greet;
-};
+import React, {Component} from 'react'
+import config from './config.json';
+import styles from './Greeter.css';//导入
+
+class Greeter extends Component{
+    render() {
+        console.log('render!!!');
+        return (
+            <div className={styles.root}>
+                {config.greetText}
+            </div>
+        );
+    }
+}
+
+export default Greeter;
